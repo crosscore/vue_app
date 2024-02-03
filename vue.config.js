b5de4774d254b79ec36b095b6e5d6e28a08540e8
yuu <crosscore.g@gmail.com>
2024-02-03 21:54:10 +0900

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
+// vue_app/vue.config.js
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const fs = require("fs");
+module.exports = {
+  devServer: {
+    https: {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    host: "0.0.0.0",
+    port: 8080,
+  },
+};
